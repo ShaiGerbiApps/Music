73467edfb57aeabaed026acb2e487ce8fb8aabcb
Shai Gerbi <ShaiGerbiApps@gmail.com>
2021-08-25 20:42:06 +0300

    Add ad banner

--- app/src/main/java/com/shaigerbi/retromusic/activities/SupportDevelopmentActivity.kt
+++ app/src/main/java/com/shaigerbi/retromusic/activities/SupportDevelopmentActivity.kt
@@ -15,20 +15,47 @@
 package com.shaigerbi.retromusic.activities
 
 import android.os.Bundle
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.shaigerbi.appthemehelper.util.ATHUtil
 import com.shaigerbi.appthemehelper.util.ToolbarContentTintHelper
 import com.shaigerbi.retromusic.R
 import com.shaigerbi.retromusic.activities.base.AbsBaseActivity
 import kotlinx.android.synthetic.main.activity_donation.*
 
+const val AD_UNIT_ID = "ca-app-pub-1173887754649201/4970804580"
 
 class SupportDevelopmentActivity : AbsBaseActivity(){
 
-
+    private var mInterstitialAd: InterstitialAd? = null
+    private var TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_donation)
+
+        // Initialize the Mobile Ads SDK.
+        MobileAds.initialize(this) {}
+
+
+        val adRequest = AdRequest.Builder().build()
+
+        InterstitialAd.load(
+                this, AD_UNIT_ID, adRequest,
+                object : InterstitialAdLoadCallback() {
+                    override fun onAdFailedToLoad(adError: LoadAdError) {
+                        mInterstitialAd = null
+                    }
+
+                    override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                        mInterstitialAd = interstitialAd
+                        showInterstitial()
+                    }
+                }
+        )
+
+
 
         setStatusbarColorAuto()
         setNavigationbarColorAuto()
@@ -37,6 +64,27 @@ class SupportDevelopmentActivity : AbsBaseActivity(){
 
         setupToolbar()
     }
+
+
+    private fun showInterstitial() {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.fullScreenContentCallback = object : FullScreenContentCallback() {
+                override fun onAdDismissedFullScreenContent() {
+                    mInterstitialAd = null
+                }
+
+                override fun onAdFailedToShowFullScreenContent(adError: AdError?) {
+                    mInterstitialAd = null
+                }
+
+                override fun onAdShowedFullScreenContent() {
+                }
+            }
+            mInterstitialAd?.show(this)
+        }
+
+    }
+
 
     private fun setupToolbar() {
         val toolbarColor = ATHUtil.resolveColor(this, R.attr.colorSurface)
